fd8693a4674c6c0044546c1b9f585767bf7e311d
Bamazaev <olimjonmi6@gmail.com>
2026-08-22 02:26:34 +0500

    Update client: multi-category display support

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3127297624" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1540638049" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
