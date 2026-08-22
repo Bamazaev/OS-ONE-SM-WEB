b9dd7a4912830728312a420e147f2b48785de56c
Bamazaev <olimjonmi6@gmail.com>
2026-08-22 09:52:23 +0500

    Fix category chip id collision and add getCatalog fallback for un-updated deployments

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2840659784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "565158133" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
