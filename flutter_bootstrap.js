00cfdf9cc5f6bad9e8d2b553955ad614ef900bac
Bamazaev <olimjonmi6@gmail.com>
2026-08-22 09:14:56 +0500

    Add shimmer skeleton loading for catalog fetch

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3079960074" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "476235286" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
